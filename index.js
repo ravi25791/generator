ec9618350a62e21ebf7d9fe355d65b018dce3693
Ravi Kumar <ravi.kumar4rawat@gmail.com>
2023-09-16 15:05:22 +0530

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello generator function")
+/* ------- A generator function can pause and resume of code execution ------- */
+function* generatorFun(){
+    let id=100;
+    while(true){
+    id++
+    yield id;
+    }
+}
+
+let gen=generatorFun()
+
+function generateID(){
+    document.getElementById("newid").innerText=gen.next().value;
+}
